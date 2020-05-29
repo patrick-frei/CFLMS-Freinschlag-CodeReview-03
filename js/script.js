5dff8008cee31552b653c03cd5458ba32febab9f
Patrick Freinschlag <patrick.freinschlag@gmail.com>
2020-05-29 16:25:53 +0200

    adds calculator and fixes bugs

--- js/script.js
+++ js/script.js
@@ -1,12 +1,35 @@
-function calculateInsurance(country, horsePower, age) {
+function calculateInsurance(country, horsepower, age) {
   switch (country) {
     case "Austria":
-      return horsePower * 100 / age + 50
+      return `Your quote is ${Math.round((horsepower * 100 / age + 50)*100)/100}€ per year.`
       break
     case "Hungary":
-      return horsePower * 120 / age + 100
+      return `Your quote is ${Math.round((horsepower * 120 / age + 100)*100)/100}€ per year.`
       break
     case "Greece":
-      return horsePower * 150 / (age + 3) + 50
+      return `Your quote is ${Math.round((horsepower * 150 / (age + 3) + 50)*100)/100}€ per year.`
   }
 }
+
+document.getElementById("calculate").addEventListener("click", function(a){
+    if (document.getElementById("fullname").value != "") {
+      fullName = document.getElementById("fullname").value
+    }else {
+      document.getElementById("result").innerHTML = "Your name is required."
+      return
+    }
+    if (document.getElementById("age").value != "") {
+      age = parseInt(document.getElementById("age").value)
+    }else {
+      document.getElementById("result").innerHTML = "Your age is required."
+      return
+    }
+    if (document.getElementById("horsepower").value != "") {
+      horsepower = parseInt(document.getElementById("horsepower").value)
+    }else {
+      document.getElementById("result").innerHTML = "Your car's horsepower is required."
+      return
+    }
+    country = document.getElementById("country").value
+    document.getElementById("result").innerHTML = calculateInsurance(country, horsepower, age)
+});
